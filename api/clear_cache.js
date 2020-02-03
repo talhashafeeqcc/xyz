a8cf3f215655f12d122335a2d2c4d744012be3cc
dbauszus-glx <dennis.bauszus@geolytix.co.uk>
2020-02-03 17:32:56 +0000

    add token to clear cache.

--- api/clear_cache.js
+++ api/clear_cache.js
@@ -7,9 +7,9 @@ module.exports = (req, res) => auth(req, res, handler, {
   login: true
 });
 
-async function handler(req, res){
+async function handler(req, res, token={}){
 
-  fetch(`${req.headers.host.includes('localhost') && 'http' || 'https'}://${req.headers.host}${process.env.DIR || ''}/api/workspace/get`,
+  fetch(`${req.headers.host.includes('localhost') && 'http' || 'https'}://${req.headers.host}${process.env.DIR || ''}/api/workspace/get?token=${token.signed}`,
   { headers: new fetch.Headers({ 'Cache-Control': 'no-cache' }) })
 
   res.send('cache cleared');
