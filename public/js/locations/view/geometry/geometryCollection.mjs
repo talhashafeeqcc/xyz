385b00746089d60c4dfa5da89acb8288f3e1a8ae
Agata Brok <agatbr@gmail.com>
2020-03-23 18:19:09 +0000

    Geometry collection - adjustments in case styling is unset.

--- public/js/locations/view/geometry/geometryCollection.mjs
+++ public/js/locations/view/geometry/geometryCollection.mjs
@@ -1,51 +1,60 @@
 export default _xyz => entry => {
 
-	entry.value.features.map(feature => {
+    entry.value.features.map(feature => {
 
-		let style;
+        let style;
 
-		if(entry.style.theme.type === 'categorized') style = entry.style.theme && entry.style.theme.cat[feature.properties[entry.style.theme.field]].style;
+        if (!entry.style) style = entry.location.style;
 
-        if(entry.style.theme.type === 'graduated') {
+        if (!entry.style.theme) style = entry.style;
 
-        	for (let i = 0; i < entry.style.theme.cat_arr.length; i++) {
+        //console.log(entry);
 
-        		if (feature.properties[entry.style.theme.field] < entry.style.theme.cat_arr[i].value) break;
+        if (entry.style.theme && entry.style.theme.type === 'categorized') style = entry.style.theme && entry.style.theme.cat[feature.properties[entry.style.theme.field]].style;
 
-        		style = entry.style.theme.cat_arr[i].style;
+        if (entry.style.theme && entry.style.theme.type === 'graduated') {
+
+            for (let i = 0; i < entry.style.theme.cat_arr.length; i++) {
+
+                if (feature.properties[entry.style.theme.field] < entry.style.theme.cat_arr[i].value) break;
+
+                style = entry.style.theme.cat_arr[i].style;
             }
 
-          }
+        }
 
-          let f = _xyz.mapview.geoJSON({
+        let f = _xyz.mapview.geoJSON({
             geometry: feature.geometry,
             dataProjection: '4326',
-            zIndex: entry.location.layer.L.getZIndex()-1,
+            zIndex: entry.location.layer.L.getZIndex() - 1,
             style: new _xyz.mapview.lib.style.Style({
-              stroke: style.strokeColor && new _xyz.mapview.lib.style.Stroke({
-                color: _xyz.utils.Chroma(style.color || style.strokeColor).alpha(1),
-                width: entry.style.strokeWidth || 1
-              }),
-              fill: new _xyz.mapview.lib.style.Fill({
-                color: _xyz.utils.Chroma(style.fillColor || style.strokeColor).alpha(style.fillOpacity === undefined ? 1 : parseFloat(style.fillOpacity) || 0).rgba()
-              })
+                stroke: style.strokeColor && new _xyz.mapview.lib.style.Stroke({
+                    color: _xyz.utils.Chroma(style.color || style.strokeColor).alpha(1),
+                    width: entry.style.strokeWidth || 1
+                }),
+                fill: new _xyz.mapview.lib.style.Fill({
+                    color: _xyz.utils.Chroma(style.fillColor || style.strokeColor).alpha(style.fillOpacity === undefined ? 1 : parseFloat(style.fillOpacity) || 0).rgba()
+                })
             })
-          });
-
-          entry.location.geometryCollection.push(f);
-
         });
 
-	entry.location.geometries.push(entry.location.geometryCollection);
+        entry.location.geometryCollection.push(f);
+
+    });
+
+    entry.location.geometries.push(entry.location.geometryCollection);
     entry.display = true;
 
-  entry.legend = _xyz.utils.wire()`<div class="legend lv-1">`;
+    if (!entry.style || !entry.style.theme) return;
 
-  entry.container.appendChild(entry.legend);
+    entry.legend = _xyz.utils.wire()
+    `<div class="legend lv-1">`;
 
-  entry.legend.appendChild(_xyz.layers.view.style.legend({
-    format: 'mvt',
-    style: entry.style
-  }));
+    entry.container.appendChild(entry.legend);
+
+    entry.legend.appendChild(_xyz.layers.view.style.legend({
+        format: 'mvt',
+        style: entry.style
+    }));
 
 }
